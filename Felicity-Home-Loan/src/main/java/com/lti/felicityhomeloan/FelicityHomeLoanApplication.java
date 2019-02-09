5ca945e0839155d3fc88e32024aaa8fea0b8051a
Tejas-Choudekar <tejus.choudekar@gmail.com>
2019-02-09 11:42:14 +0530

    Home loan Project

---
+++ Felicity-Home-Loan/src/main/java/com/lti/felicityhomeloan/FelicityHomeLoanApplication.java
@@ -0,0 +1,14 @@
+package com.lti.felicityhomeloan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FelicityHomeLoanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FelicityHomeLoanApplication.class, args);
+	}
+
+}
+
---
+++ Felicity-Home-Loan/src/test/java/com/lti/felicityhomeloan/FelicityHomeLoanApplicationTests.java
@@ -0,0 +1,17 @@
+package com.lti.felicityhomeloan;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class FelicityHomeLoanApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
